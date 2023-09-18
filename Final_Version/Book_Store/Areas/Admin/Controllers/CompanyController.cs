a646fb101d1382b154a8b46394d559b5d829c8d9
akuklishin <a.kuklishin@gmail.com>
2023-09-18 09:47:17 -0400

    Final_Version Added

---
+++ Final_Version/Book_Store/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Store.DataAccess.Repository.IRepository;
+using Store.Models;
+using Store.Models.ViewModels;
+using Store.Utility;
+
+namespace Book_Store.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id) //Update + Insert
+        {
+            Company Company = new Company(); 
+            if (id == null || id == 0)
+            {
+                //create
+                return View(Company);
+            }
+            else
+            {
+                //update
+                Company = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(Company);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company CompanyObj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (CompanyObj.Id == null || CompanyObj.Id == 0)
+                {
+                    //create
+                    _unitOfWork.Company.Add(CompanyObj);
+                    /*Success flash message*/
+                    TempData["Success"] = "Company created successfully";
+
+                }
+                else
+                {
+                    //update
+                    _unitOfWork.Company.Update(CompanyObj);
+                    /*Success flash message*/
+                    TempData["Success"] = "Company updated successfully";
+                }
+
+                _unitOfWork.Save();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(CompanyObj);
+            }
+            
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompanyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            var companyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+            if (companyToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(companyToBeDeleted);
+            _unitOfWork.Save();
+
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ Final_Version/Book_Store/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.DataAccess.Repository.IRepository;
+using Store.Models;
+using Store.Utility;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Book_Store.Areas.Customer.Controllers
+{
+    //The above path was automaticly adjusted by pressing "OK" in the pop up window when moving file to area
+    //But there is one more option to guide to the area:
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+
+            //Id for display current cart when logging in
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim != null)
+            {
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+            }
+
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages"),
+                Count = 1,
+                ProductId = productId
+            };
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+
+            if (cartFromDb != null)
+            {
+                //shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                //add cart record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+
+                //session for cart
+                HttpContext.Session.SetInt32(SD.SessionCart, 
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
+            }
+
+            TempData["success"] = "Cart updated successfully";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
