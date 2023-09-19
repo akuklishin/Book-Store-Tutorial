d78d0cb76a6c52466b577e9e1971e19271c5192a
akuklishin <a.kuklishin@gmail.com>
2023-09-18 20:07:07 -0400

    Added Sections 1-2 versions

---
+++ Section_2_Category_CRUD_Operations/Version_2.0/BulkyWeb/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyWeb.Controllers
+{
+    // HomeController: Controller for managing the default views like Index, Privacy, and Error.
+    public class HomeController : Controller
+    {
+        // Logger instance used to log diagnostic information.
+        private readonly ILogger<HomeController> _logger;
+
+        // Constructor that initializes the logger via Dependency Injection.
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Index action: Returns the main landing page (typically the homepage) of the website.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Privacy action: Returns the Privacy policy page of the website.
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Error action: Returns a generic error page. Useful for displaying error details in a user-friendly manner.
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Section_2_Category_CRUD_Operations/Version_2.0/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // whatever options we configure here will be passed on to the base class of DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
